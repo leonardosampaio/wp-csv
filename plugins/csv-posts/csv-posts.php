804fbe6e0942f1d8c3c2b4f8aeb63286fea99b34
Leonardo Sampaio <leonardosampaio@mpma.mp.br>
2021-05-13 11:29:12 -0300

    yoast meta

--- plugins/csv-posts/csv-posts.php
+++ plugins/csv-posts/csv-posts.php
@@ -2,13 +2,13 @@
 /*
 Plugin Name: CSV Posts
 Plugin URI: https://www.upwork.com/fl/leonardorsampaio
-Description: Inserir posts em lote a partir de um arquivo CSV com colunas TAG_NOME e TAG_IMAGEM
+Description: Inserir posts em lote a partir de um arquivo CSV com colunas TAG_NOME e zero ou mais TAG_IMAGEM
 Author: Leonardo Sampaio
 Version: 1.0
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
-	die( 'Kangaroos cannot jump here' );
+	die();
 }
 
 function csv_meta_box_add()
@@ -21,19 +21,19 @@ function csv_meta_box_add()
     }
 
     add_meta_box(
-            'csv-posts-meta',
-            'CSV Posts',
-            'html',
-            'post',
-            'normal',
-            'high');
+        'csv-posts-meta',
+        'CSV Posts',
+        'csv_meta_box_html',
+        $post->post_type,
+        'normal',
+        'high');
 }
 
-function html( $post )
+function csv_meta_box_html( $post )
 {
     wp_nonce_field( '_csv_posts_nonce', 'csv_posts_nonce' ); ?>
     <p>Para publicar posts em lote, informe um arquivo CSV com uma coluna TAG_NOME e
-    uma ou mais colunas TAG_IMAGEM, que serão substituídas no texto.</p>
+    zero ou mais colunas TAG_IMAGEM, que serão substituídas no texto. Cada linha corresponderá a um novo post.</p>
     <label for="csv-posts-visibily">Visibilidade</label>
     <select id="csv-posts-visibily" name="csv-posts-visibily">
         <option value="publish">Público</option>
@@ -46,27 +46,19 @@ function html( $post )
     <?php
 }
 
-function admin_head_js() {
-echo "
-<script>
-    window.addEventListener('load', (event) => {
-    document.getElementById('csv-input').onchange = function(e) {
-            console.log(e);
-    };
-    });
-</script>
-";
-}
-
 function hook_transition_post_status($new_status, $old_status, $post)
 {
+    set_transient('csv-count', null, 100);
+    
     if((!isset($_POST['csv_import']) && !$_POST['csv_import']) &&
-        in_array($new_status,['draft','private']) && 
-        isset($_FILES['csv-input']) && 
-        isset($_FILES["csv-input"]["tmp_name"]))
+    in_array($new_status,['draft','private']) && 
+    isset($_FILES['csv-input']) && 
+    isset($_FILES["csv-input"]["tmp_name"]))
     {
+        $yoastActive = (is_plugin_active( 'wordpress-seo/wp-seo.php' ) || 
+            is_plugin_active( 'wordpress-seo-premium1/wp-seo-premium.php' ));
+
         $count = 0;
-        set_transient('csv-count', 0, 100);
 
         $csvFileHandler = fopen($_FILES["csv-input"]["tmp_name"],"r");
         if ($csvFileHandler)
@@ -93,7 +85,7 @@ function hook_transition_post_status($new_status, $old_status, $post)
                             foreach($imgs[1] as $k => $img)
                             {
                                 $productImage = $productImages[$k] ? $productImages[$k] : $productImages[0];
-                                $replacedContent = str_replace($img, $productImage, $replacedContent);
+                                $replacedContent = str_replace($img, trim($productImage), $replacedContent);
                             }
                         }
                     }
@@ -105,27 +97,43 @@ function hook_transition_post_status($new_status, $old_status, $post)
                     
                     $replacedTitle = str_replace('TAG_NOME', $productName, $post->post_title);
     
-                    $replacedName = preg_replace("/[^A-Za-z0-9_]/", '', 
-                        str_replace(' ', '_', $productName));
+                    $replacedName = preg_replace("/[^A-Za-z0-9-]/", '', 
+                        str_replace(' ', '-', $productName));
+
+                    $postName = str_replace('tag_nome', $replacedName, strtolower($post->post_name));
 
                     $newPost = [
-                        "post_title"    =>  $replacedTitle,
-                        "post_name"     =>  $replacedName,
-                        "post_content"  =>  $replacedContent,
-                        "post_status"   =>  $postsVisibility,
-                        "post_date"     =>  $dateStr,
-                        "post_modified" =>  $dateStr
+                        "post_type"             =>  $post->post_type,
+                        "post_title"            =>  $replacedTitle,
+                        "post_name"             =>  $postName,
+                        "post_content"          =>  $replacedContent,
+                        "post_status"           =>  $postsVisibility,
+                        "post_date"             =>  $dateStr,
+                        "post_modified"         =>  $dateStr
                     ];
 
                     $_POST['csv_import'] = true;
-                    if (is_int(wp_insert_post($newPost, true, true)))
+
+                    $id = wp_insert_post($newPost, true, true);
+                    if (is_int($id))
                     {
+                        if ($yoastActive)
+                        {
+                            $metaTitle = str_replace('TAG_NOME', $productName, $_POST['yoast_wpseo_title']);
+                            update_post_meta($id, '_yoast_wpseo_title', $metaTitle);
+
+                            $metaDescription = str_replace('TAG_NOME', $productName, $_POST['yoast_wpseo_metadesc']);
+                            update_post_meta($id, '_yoast_wpseo_metadesc',  $metaDescription);
+                            
+                            $metaKeyword = str_replace('TAG_NOME', $productName, $_POST['yoast_wpseo_focuskw']);
+                            update_post_meta($id, '_yoast_wpseo_focuskw',   $metaKeyword);
+                        }
                         $count++;
                     }
                 }
             }
             fclose($csvFileHandler);
-            set_transient('csv-count', $count, 100);
+            set_transient('csv-count', [$count, $postsVisibility], 100);
         }
     }
 }
@@ -139,16 +147,28 @@ function admin_notice() {
 
     $count = get_transient('csv-count');
 
-    if (!$count || $count === 0)
+    if (!$count || 
+        !$count[0] || 
+        !$count[1] || 
+        $count[0] === 0)
     {
         return;
     }
 
+    $multiple = $count[0] > 1 ? 's' : '';
+
     echo '<div class="notice notice-success is-dismissible"><p>';
-    echo sprintf( __( '%s posts publicados a partir do arquivo CSV' ), $count );
+    echo sprintf(__( '%s post%s publicado%s como %s a partir do arquivo CSV' ),
+        $count[0],
+        $multiple,
+        $multiple,
+        ($count[1] === 'publish' ? 'Público' : 
+            ($count[1] === 'private' ? 'Privado' : 
+                'Rascunho'))
+        );
     echo '</p></div>';
 
-    set_transient('csv-count', 0, 100);
+    set_transient('csv-count', null, 100);
 }
 
 function add_post_multipart_form_data() {
@@ -166,5 +186,4 @@ add_action('post_edit_form_tag', 'add_post_multipart_form_data');
 add_action('save_draft', 'save_draftdata');
 add_action('save_post', 'save_postdata');
 add_action('add_meta_boxes', 'csv_meta_box_add');
-add_action('admin_head', 'admin_head_js');
 add_action('transition_post_status', 'hook_transition_post_status', 10, 3);
