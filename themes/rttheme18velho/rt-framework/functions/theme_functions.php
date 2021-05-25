6113eec9543557956cf7964efb94c6b468819efb
Leonardo Sampaio <leonardosampaio@mpma.mp.br>
2021-05-25 18:46:30 -0300

    meta aninhada com div

--- themes/rttheme18velho/rt-framework/functions/theme_functions.php
+++ themes/rttheme18velho/rt-framework/functions/theme_functions.php
@@ -2541,13 +2541,13 @@ if( ! function_exists("rt_structured_data") ){
 			<meta itemprop="mainEntityOfPage" itemscope itemtype="http://schema.org/WebPage">
 			<meta itemprop="dateModified" content="'. get_the_modified_date('Y-m-d') .'">
 			<meta itemprop="headline" content="'. $rt_global_post_values["title"] .'">
-			<meta itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
+			<div itemprop="publisher" itemscope itemtype="https://schema.org/Organization" style="display:none;">
 					<meta itemprop="name" content="Mokka Com. de Bens de Consumo">
-					<meta itemprop="logo" itemscope itemtype="https://schema.org/ImageObject">
+					<div itemprop="logo" itemscope itemtype="https://schema.org/ImageObject" style="display:none;">
 							<meta itemprop="url" content="https://mokka-sensors.com.br/wp-content/uploads/2020/01/Logo-Mokka-Sensors.png">
-					</meta>
+					</div>
 
-			</meta>
+			</div>
 		';
 
 	}
